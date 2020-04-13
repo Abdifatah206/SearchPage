a024d34726ff6f511502b6cf384e2af1cdd11c85
Abdifatah.206 <shire2567@gmail.com>
2020-04-13 10:07:25 -0700

    table added

---
+++ search-page/src/Users.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import axios from 'axios';
+import postdata from './data.json';
+import './App.css';
+
+
+
+function searchFor(term){
+  return function(x){
+    return x.title.toLowerCase().includes(term.toLowerCase()) || !term;
+  }
+}
+export default class Users extends React.Component {
+  constructor(props){
+     super(props);
+     this.state={
+       postdata : postdata,
+       term : '',
+     }
+     this.searchHandler = this.searchHandler.bind(this);
+   }
+   searchHandler(event){
+     this.setState({term: event.target.value})
+   }
+
+
+
+
+  render() {
+  const {term , postdata} = this.state;
+
+      return (
+        <div style={{alignitems:'center'}}>
+        <div> <h1> Quze Course List </h1></div>
+        <form>
+        Search-by-course : <input type="text"  onChange={this.searchHandler} value={term} />
+        </form>
+        <tr >
+        <th>CourseID</th>
+        <th>Title</th>
+        <th>Duration</th>
+        <th>Category</th>
+
+        <th>More</th>
+        </tr>
+        {postdata.filter(searchFor(term)).map((postdetial, index) =>{
+          return <div >
+
+
+              <td>{postdetial.courseId}</td>
+              <td>{postdetial.title}</td>
+              <td>{postdetial.duration}</td>
+              <td>{postdetial.category}</td>
+
+              <td><button style={{
+                background: 'white',
+                color: 'black',
+                padding: '10px 20px',
+                fontsize: '16px',
+                cursor: 'pointer',
+                float:'left',
+                marginright: '100px',
+                display:'inline',
+
+                }}>Detail</button></td>
+
+
+
+
+              </div>
+
+
+        })}
+
+
+        </div>
+
+      );
+    }
+  }
